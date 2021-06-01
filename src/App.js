dcbcceb3c72782864f26f88f32af2d23dbf01cf1
Angelica Esposo <73009213+enjelesposo@users.noreply.github.com>
2021-06-01 17:52:53 +0800

    Initial upload

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, {useState, useEffect} from 'react';
+import Recipe from './Recipe'
+import './App.css';
+
+function App() {
+  const APP_ID = 'f650d73a';
+  const APP_KEY = 'd7a2a32f9e23862f5f8663f49654d078';
+
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    getRecipes();
+  },[]);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    setRecipes(data.hits);
+    console.log(data.hits)
+  }
+
+  // update value on input
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  }
+
+  const getSearch = e => {
+    
+  }
+
+  return (
+    <div className="App">
+      <form className='search-form'>
+        <input className='searc-bar' type="text" value={search} onChange={updateSearch}/>
+        <button type='submit'>
+          <i className="fa fa-search" aria-hidden="true"></i>
+        </button>
+      </form>
+      {
+        recipes.map( recipe => {
+          return <Recipe key={recipe.recipe.label} title={recipe.recipe.label} calories={recipe.recipe.calories} image={recipe.recipe.image}/>
+        })
+      }
+    </div>
+  );
+}
+
+export default App;
